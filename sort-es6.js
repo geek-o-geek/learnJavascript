7cf96b4d4215cd774652cbb87633b0065498103a
NewsOne <arjun.sisodia@gmail.com>
2018-06-26 21:37:34 +0530

    sort-es6 chnage

--- sort-es6.js
+++ sort-es6.js
@@ -3,9 +3,10 @@ const sort = (arr) => {
   	for(let j=0; j<i; j++){
       if(arr[i] < arr[j]){
         [arr[i], arr[j]] = [arr[j], arr[i]];
-        console.log(arr);
+        return arr;
     	}
     }  
   }
 }
-sort([5,2,1,8]);
+array_to_sort = [5, 2, 1, 8];
+console.log(sort(array_to_sort);
