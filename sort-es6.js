003d44513e8004e88d391a28fc751cf07385daef
NewsOne <arjun.sisodia@gmail.com>
2018-06-26 21:40:43 +0530

    sort-es6

--- sort-es6.js
+++ sort-es6.js
@@ -1,12 +1,13 @@
+
 const sort = (arr) => {
   for(let i=1; i<arr.length; i++){
   	for(let j=0; j<i; j++){
       if(arr[i] < arr[j]){
         [arr[i], arr[j]] = [arr[j], arr[i]];
-        return arr;
     	}
     }  
   }
+  return arr;
 }
-array_to_sort = [5, 2, 1, 8];
-console.log(sort(array_to_sort);
+const array_to_sort = [5, 2, 1, 8];
+console.log(sort(array_to_sort));
